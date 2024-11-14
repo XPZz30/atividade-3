6a91c8bf91d6727a77c4918277b3634cd81b2e2e
Samuel <samuleal007@gmail.com>
2024-11-14 17:45:06 -0300

    atividade 3

--- server.js
+++ server.js
@@ -5,26 +5,19 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-// Middleware para processar dados do formulário
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Lista para armazenar as empresas cadastradas
 const empresas = [];
 
-// Rota para exibir o formulário e a lista de empresas cadastradas
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Rota para processar o cadastro de empresas
 app.post('/cadastrar', (req, res) => {
   const { cnpj, razaoSocial, nomeFantasia, endereco, cidade, uf, cep, email, telefone } = req.body;
-
-  // Validação dos campos
   const camposVazios = [];
+
   if (!cnpj) camposVazios.push('CNPJ');
   if (!razaoSocial) camposVazios.push('Razão Social');
   if (!nomeFantasia) camposVazios.push('Nome Fantasia');
@@ -39,19 +32,14 @@ app.post('/cadastrar', (req, res) => {
     return res.send(`Os seguintes campos são obrigatórios: ${camposVazios.join(', ')}`);
   }
 
-  // Adicionar a nova empresa à lista
   empresas.push({ cnpj, razaoSocial, nomeFantasia, endereco, cidade, uf, cep, email, telefone });
-
-  // Redirecionar de volta para o formulário com a lista atualizada
   res.redirect('/');
 });
 
-// Rota para fornecer a lista de empresas cadastradas para o front-end
 app.get('/empresas', (req, res) => {
   res.json(empresas);
 });
 
-// Iniciando o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
